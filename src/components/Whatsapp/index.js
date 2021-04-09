890e560d7e15f0f4c2024faae5f4d5d64e4be481
Mateus Campos <mateus_98@hotmail.com>
2021-04-09 00:03:29 -0300

    change link for redirect chat in whatsapp

--- src/components/Whatsapp/index.js
+++ src/components/Whatsapp/index.js
@@ -32,7 +32,7 @@ a {
 export default function Whatsapp(props) {
   return(
     <Icon>
-      <a href="https://web.whatsapp.com/send?phone=5511987079920&text=Olá!%20gostaria%20de%20saber%20mais%20informações%20sobre%20o%20rastreamento%20veícular." target="_blank">
+      <a href="https://api.whatsapp.com/send?phone=5511987079920&text=Olá!%20gostaria%20de%20saber%20mais%20informações%20sobre%20o%20rastreamento%20veícular." target="_blank">
         <FontAwesomeIcon icon={faWhatsapp}></FontAwesomeIcon>
       </a>
     </Icon>
